1dfc4f520fd5e0b5603057fded1081d911a31ea8
Yaseen <yaseen.nvt@gmail.com>
2020-11-23 18:00:33 -0500

    shiny new junit 5

--- src/test/java/UrbitTests.java
+++ src/test/java/UrbitTests.java
@@ -1,17 +1,26 @@
-import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 
 public class UrbitTests {
-	private Urbit ship;
+	private static Urbit ship;
 
-	@BeforeClass
+	@BeforeAll
 	public static void setup() {
 		String url = "http://localhost:80";
 		String shipName = "zod";
 		String code = "lidlut-tabwed-pillex-ridrup";
 
 		ship = new Urbit(url, shipName, code);
+	}
+
+	@Test
+	@Order(1)
+	public void shipConnects() throws Exception {
 		ship.connect();
 	}
+
+
 
 
 }
