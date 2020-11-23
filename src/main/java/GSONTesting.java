ff28e9ce71276acbfb6f5dbf368b672522623170
Yaseen <yaseen.nvt@gmail.com>
2020-11-23 17:13:53 -0500

    Add file to experiment with using GSON in an easier way.

---
+++ src/main/java/GSONTesting.java
@@ -0,0 +1,32 @@
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GSONTesting {
+
+	public static void main(String[] args) {
+		Map<String, Object> data = new HashMap<>();
+		data.put("ship", "~sipfyn-pidmex");
+		data.put("app", "hood");
+		data.put("mark", "helm-hi");
+		data.put("json", "Opening airlock");
+
+
+		Map<String, Object> payload = new HashMap<>();
+		payload.put("id", 1);
+		payload.put("action", "poke");
+
+		payload.putAll(data);
+
+
+		Gson gson = new Gson();
+		String json = gson.toJson(payload);
+		System.out.println("Resulting json:");
+		System.out.println(json);
+
+
+
+	}
+
+}
