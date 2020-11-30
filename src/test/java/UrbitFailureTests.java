19dfa72268ce42b6840a048ffddc3aa8bde3152e
Yaseen <yaseen.nvt@gmail.com>
2020-11-30 18:00:45 -0500

    Create UrbitFailureTests

---
+++ src/test/java/UrbitFailureTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class UrbitFailureTests {
+
+	// TODO populate with tests which test various failure modes of the api
+
+	@BeforeAll
+	public static void setup() {
+
+	}
+
+}
