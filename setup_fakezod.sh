06fa58cd0501020d33a60c774fcb51ccb9012ff9
Yaseen <yaseen.nvt@gmail.com>
2020-11-30 20:21:03 -0500

    more work on fakezod script

--- setup_fakezod.sh
+++ setup_fakezod.sh
@@ -5,10 +5,20 @@ cd urbit || exit
 curl -O https://bootstrap.urbit.org/urbit-v0.10.8-linux64.tgz
 tar xzf urbit-v0.10.8-linux64.tgz
 cd ./urbit-v0.10.8-linux64/ || exit
-screen -d -m -S fakezod ./urbit -F zod
+
+#PATP=${1:-"zod"}
+#PATP="zod"
+
+screen -d -m -S fakeship -L -Logfile "./fakeship_output.log" ./urbit -F zod
+# shellcheck disable=SC2076
+until [[ "$(tail -n1 fakeship_output.log)" =~ "~zod:dojo>" ]]; do
+  echo "Waiting for zod to boot"
+  sleep 10s
+done
+
 # from https://raymii.org/s/snippets/Sending_commands_or_input_to_a_screen_session.html
 # https://urbit.org/using/operations/using-your-ship/#chat-management
-screen -S fakezod -p 0 -X stuff "^X"
-screen -S fakezod -p 0 -X stuff ";create channel /test"
+screen -S fakeship -p 0 -X stuff "^X"
+screen -S fakeship -p 0 -X stuff ";create channel /test"
 # assuming eyre will be live on 8080
 # todo use urbit/herb
