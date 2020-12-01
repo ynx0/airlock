c422a31c8e843184c17aba1a99508ec601c4552b
Yaseen <yaseen.nvt@gmail.com>
2020-11-30 19:00:55 -0500

    Improve `Main`

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,61 +1,49 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
 
-import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("BusyWait")
 public class Main {
 
 	private static final Gson gson = new Gson();
 	private static final List<SubscribeEvent> chatStoreEvents = new ArrayList<>();
-	private static final List<SubscribeEvent> primaryChatViewEvents = new ArrayList<>();
+	private static final List<SubscribeEvent> chatViewEvents = new ArrayList<>();
 
 	public static void main(String[] args) throws Exception {
 		String url = "http://localhost:8080";
 		String shipName = "zod";
 		String code = "lidlut-tabwed-pillex-ridrup";
 
+
+		// SETUP
+		// The following example assumes you have:
+		// - a ship named 'zod' running
+		// - a chat channel called 'test' (you must manually create this)
+
+
+		// MARK - ship setup
 		Urbit ship = new Urbit(url, shipName, code);
-		ship.authenticate();
+		ship.authenticate(); // submit the code to the ship for authentication. must be done before anything else
+		ship.connect();      // establishes the ServerSideEvent (SSE) client. this is what is used to receive all responses from the ship
 
-		Main.test1(ship); // successful
-		Main.test2(ship); // successful
-		Main.test3(ship); // successful
-
-		while (chatStoreEvents.size() < 2) {
-			Thread.sleep(50);
-		}
-		System.out.println("finished. got the following events");
-		System.out.println(chatStoreEvents);
-
-		Main.testChatView(ship); // successful
-
-		// todo possibly test manually canceling event source / deleting channel
-
-	}
-
-
-	public static void test1(Urbit ship) throws IOException {
-		ship.connect();
-	}
-
-	public static void test2(Urbit ship) throws IOException {
-		int subscriptionID = ship.subscribe(ship.getShipName(), "chat-store", "/mailbox/~zod/test2", subscribeEvent -> {
+		// MARK - create a 'mailbox' subscription on the chat-store gall agent
+		int chatStoreSubscriptionID = ship.subscribe(ship.getShipName(), "chat-store", "/mailbox/~zod/test", subscribeEvent -> {
 			System.out.println("[Subscribe Event]");
 			System.out.println(subscribeEvent);
+			// store each event to a list
 			chatStoreEvents.add(subscribeEvent);
 		});
-		System.out.println("Chat Store Subscription ID: " + subscriptionID);
-	}
 
-	public static void test3(Urbit ship) throws IOException {
+
+		// MARK - Send a chat message on the channel 'test'
+		// create the json payload
 		Map<String, Object> payload = Map.of(
 				"message", Map.of(
-						"path", "/~zod/test2",
+						"path", "/~zod/test",
 						"envelope", Map.of(
 //								"uid", Urbit.uid(),
 								"uid", "0v1.00000.3eolm.59lvl.7n9ht.2mokl.51js7",
@@ -67,26 +55,41 @@ public class Main {
 				)
 		);
 
+		// convert json payload to JsonElement
 		JsonElement payloadJSON = gson.toJsonTree(payload);
-		ship.poke(ship.getShipName(), "chat-hook", "json", payloadJSON, pokeEvent -> {
-			if (pokeEvent.success) {
-				System.out.println("[PokeHandler]: successfully poked message to mailbox");
-			} else {
-				System.out.println("[PokeHandler]: could not poke message to mailbox");
-				System.out.println("[PokeHandler]: failureMessage: ");
-				System.out.println(pokeEvent.failureMessage);
-			}
-		});
 
-	}
+		// make a poke request to the "chat-hook" app with the message as the payload, "json" being the payload type
+		ship.poke(ship.getShipName(), "chat-hook", "json", payloadJSON).whenComplete(
+				(pokeResponse, throwable) -> {
+					if (pokeResponse.success) {
+						System.out.println("[PokeHandler]: successfully poked message to mailbox");
+					} else {
+						System.out.println("[PokeHandler]: could not poke message to mailbox. Failed with message: \n" + pokeResponse.failureMessage);
+					}
+				}
+		);
 
-	public static void testChatView(Urbit ship) throws IOException {
-		int subscriptionID = ship.subscribe(ship.getShipName(), "chat-view", "/primary", subscribeEvent -> {
+		// wait for events to populate in the list
+		while (chatStoreEvents.size() < 2) {
+			Thread.sleep(50);
+		}
+		System.out.println("got the following events from chat-store, with subscription id:" + chatStoreSubscriptionID);
+		chatStoreEvents.forEach(System.out::println);
+
+		// MARK - create a subscription on the path "/primary" on the chat-view gall agent
+		int chatViewSubscriptionID = ship.subscribe(ship.getShipName(), "chat-view", "/primary", subscribeEvent -> {
 			System.out.println("[Primary Subscribe Event]");
 			System.out.println(subscribeEvent);
-			primaryChatViewEvents.add(subscribeEvent);
+			chatViewEvents.add(subscribeEvent);
 		});
-		System.out.println("Chat View Subscription ID: " + subscriptionID);
+
+		while (chatViewEvents.size() < 2) {
+			Thread.sleep(50);
+		}
+		System.out.println("go the following events from chat-store, with subscription id: " + chatViewSubscriptionID);
+		chatViewEvents.forEach(System.out::println);
+
 	}
+
 
 }
