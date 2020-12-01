df45d4d8af2787ef85a7d65bbc7a342c5035b0b0
Yaseen <yaseen.nvt@gmail.com>
2020-12-01 15:19:08 -0500

    start

---
+++ src/main/java/app/chathook/ChatMessage.java
@@ -0,0 +1,8 @@
+package app.chathook;
+
+import java.nio.file.Path;
+
+public class ChatMessage {
+	public Envelope envelope;
+	public Path path;
+}
---
+++ src/main/java/app/chathook/ChatUpdate.java
@@ -0,0 +1,7 @@
+package app.chathook;
+
+import com.google.gson.JsonObject;
+
+public class ChatUpdate {
+	public ChatMessage message;
+}
