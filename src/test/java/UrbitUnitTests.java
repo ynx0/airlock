e84e37fb548c55c95b17235c8bf7e627b6806c68
Yaseen <yaseen.nvt@gmail.com>
2020-12-06 17:53:35 -0500

    Add unit test for Resource data class

--- src/test/java/UrbitUnitTests.java
+++ src/test/java/UrbitUnitTests.java
@@ -1,4 +1,8 @@
 import airlock.Urbit;
+import airlock.agent.graph.Resource;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
@@ -6,9 +10,11 @@ import java.util.Map;
 
 public class UrbitUnitTests {
 
+	public static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
 	@Test
 	public void chatUtilsCreatesProperPayload() {
-		// todo integration tests
+		// todo unit tests
 		String uid = Urbit.uid();
 		String testText = "Hello World";
 		Map<String, Object> targetPayload = Map.of(
@@ -24,5 +30,12 @@ public class UrbitUnitTests {
 				)
 		);
 	}
+
+
+	@Test
+	public void validResourceFromClass() {
+		Assertions.assertEquals(new Resource("~zod", "test-graph"), gson.fromJson("{\"ship\": \"~zod\",\"name\": \"test-graph\"}", Resource.class));
+	}
+
 
 }
