601850bc72f7d579fece4b0c65caf3259e48d4ad
Yaseen <yaseen.nvt@gmail.com>
2020-12-08 11:38:18 -0500

    Update Agent base class

--- src/main/java/airlock/agent/Agent.java
+++ src/main/java/airlock/agent/Agent.java
@@ -1,12 +1,31 @@
 package airlock.agent;
 
+import airlock.PokeResponse;
 import airlock.Urbit;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.Objects.requireNonNullElse;
 
 public abstract class Agent {
 
-	private final Urbit urbit;
+	protected final Urbit urbit;
+
 	// adapted from https://github.com/dclelland/UrsusAPI/
 	protected Agent(Urbit urbit) {
 		this.urbit = urbit;
 	}
+
+	protected CompletableFuture<PokeResponse> action(String app, String mark, JsonObject data, String ship) throws IOException {
+		ship = requireNonNullElse(ship, this.urbit.getShipName());
+		return this.urbit.poke(ship, app, mark, data);
+	}
+
+	void unsubscribe(int subscriptionID) throws IOException {
+		this.urbit.unsubscribe(subscriptionID);
+	}
+
+
 }
