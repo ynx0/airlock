1d996d807cfc0b695394c2e947447c6d2e851928
Yaseen <yaseen.nvt@gmail.com>
2020-12-23 15:26:58 -0500

    Update Graph with initial impl - also rename some files

--- src/main/java/airlock/agent/graph/Graph.java
+++ src/main/java/airlock/agent/graph/Graph.java
@@ -1,5 +1,52 @@
 package airlock.agent.graph;
 
-public class Graph {
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.math.BigInteger;
+import java.util.Comparator;
+import java.util.TreeMap;
+
+// same as BigIntOrderedMap
+public class Graph extends TreeMap<BigInteger, Node> {
+
+	// todo write proper implementation. this is not functional right now
+
+	public static final Graph EMPTY_GRAPH = new Graph();
+
+	public Graph() {
+		super(Comparator.reverseOrder());
+	}
+
+	static class Adapter implements JsonSerializer<Graph>, JsonDeserializer<Graph> {
+
+		@Override
+		public JsonElement serialize(Graph src, Type typeOfSrc, JsonSerializationContext context) {
+			if (src.equals(EMPTY_GRAPH)) {
+				JsonObject empty = new JsonObject();
+				// todo, see if we need {"empty": null} here
+				return empty;
+			} else {
+				JsonArray serializedGraph = new JsonArray();
+				src.forEach(((bigInteger, node) -> {
+					JsonArray serializedEntry = new JsonArray();
+					JsonPrimitive serializedBigInt = new JsonPrimitive(bigInteger.toString());
+					JsonElement serializedNode = context.serialize(node);
+
+					serializedEntry.add(serializedBigInt);
+					serializedEntry.add(serializedNode);
+
+					serializedGraph.add(serializedEntry);
+				}));
+				return serializedGraph;
+			}
+		}
+
+		@Override
+		public Graph deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+			return null;
+		}
+	}
+
 
 }
