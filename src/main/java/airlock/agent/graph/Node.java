bd31ef30b9881ea9f90c3f466558d58893be157f
Yaseen <yaseen.nvt@gmail.com>
2020-12-25 16:52:33 -0500

    Node: cleanup imports

--- src/main/java/airlock/agent/graph/Node.java
+++ src/main/java/airlock/agent/graph/Node.java
@@ -1,12 +1,6 @@
 package airlock.agent.graph;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 import org.jetbrains.annotations.Nullable;
-
-import java.lang.reflect.Type;
 
 public class Node {
 
