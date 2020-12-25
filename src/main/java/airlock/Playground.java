c9389d874c3ede8f55926b67e9fc8c1d645bfd50
Yaseen <yaseen.nvt@gmail.com>
2020-12-25 16:25:58 -0500

    Playground: working `addPost`

--- src/main/java/airlock/Playground.java
+++ src/main/java/airlock/Playground.java
@@ -15,14 +15,6 @@ public class Playground {
 
 	public static void main(String[] args) throws Exception {
 
-//		URL baseURL = new URL("http://localhost:8080/~/").toURI().normalize().toURL();
-//		System.out.println(baseURL);
-//		System.out.println(baseURL.toURI().resolve("/scry//" + "app/" + "/"));
-//		System.out.println(baseURL.toURI().resolve("/~/channel/" + Urbit.generateChannelID()));
-
-		//  lidlut-tabwed-pillex-ridrup
-		// "toprus-dopsul-dozmep-hocbep"
-
 
 		String url = "http://localhost:80";
 //		String url = "http://localhost:8080";
@@ -33,47 +25,42 @@ public class Playground {
 		AirlockChannel urbit = new AirlockChannel(new URL(url), shipName, code);
 		urbit.authenticate();
 		urbit.connect();
+
 		GraphAgent agent = new GraphAgent(urbit);
 
-//		JsonArray graphKeys = agent.getKeys().getAsJsonObject()
-//				.get("graph-update").getAsJsonObject()
-//				.get("keys").getAsJsonArray();
-//		JsonObject graphOwnedByMe = StreamSupport.stream(graphKeys.spliterator(), false)
-//				.map(JsonElement::getAsJsonObject)
-//				.filter(key -> key.get("ship").getAsString().equals("sipfyn-pidmex"))
-//				.filter(key -> !key.get("name").getAsString().contains("dm--"))
-//				.filter(key -> key.get("name").getAsString().contains("2"))
-//				.findFirst().orElseThrow().getAsJsonObject();
-//
-//		System.out.println(graphKeys);
-//		System.out.println(graphOwnedByMe);
-//		JsonElement graphUpdate = agent.getGraph(ShipName.withSig(graphOwnedByMe.get("ship").getAsString()),graphOwnedByMe.get("name").getAsString());
-//		System.out.println(AirlockUtils.gson.toJson(graphUpdate));
-//
 		long NOW = Instant.now().toEpochMilli();
+		String ship = ShipName.withSig(urbit.getShipName());
+
+//		String groupName = "test-group-" + NOW;
+		String groupName = "my-own-stuff";
+//		ContactUtils.createNewGroup(urbit, groupName, new InvitePolicy(Collections.emptySet()), "Test Group", "test group description");
+
 		String graphName = "test-graph-" + NOW;
-		String shipWithSig = ShipName.withSig(urbit.getShipName());
-		Resource myOwnStuffGroup = GroupUtils.makeResource(shipWithSig, "my-own-stuff");
-		var createGraphResponse = agent.createManagedGraph(
+		Resource testGroup = GroupUtils.makeResource(ship, groupName);
+		agent.createManagedGraph(
 				graphName,
 				"Title of graph" + NOW,
 				"description",
-				myOwnStuffGroup,
+				testGroup,
 				GraphAgent.Module.CHAT
-				);
+		);
 
 
-		// you add the post to the graph name
-		CompletableFuture<PokeResponse> futurePostResponse = agent.addPost(shipWithSig, graphName, GraphAgent.createPost(shipWithSig, Collections.singletonList(new TextContent("Hello " + NOW)), null, null));
-		futurePostResponse.get();
-
+		CompletableFuture<PokeResponse> futurePostResponse = agent.addPost(
+				ship,
+				graphName,
+				GraphAgent.createPost(
+						ship,
+						Collections.singletonList(new TextContent("Hello " + NOW)),
+						null,
+						null
+				)
+		);
+		System.out.println(futurePostResponse.get().toString());
 
 
 		// notes dump
 		/*
-		https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/nio/charset/Charset.html
-		https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/nio/charset/CharsetEncoder.html
-		https://urbit.org/docs/reference/library/2i/#tap-by
 		failing to validate key "graph", on line /lib/graph-store/hoon:<[282 26].[282 39]>
 		after reading that line, it seems to me that it is failing to turn everything inside into a node
 
@@ -82,36 +69,10 @@ public class Playground {
 		https://urbit.org/docs/tutorials/ship-troubleshooting/#reset-code lmao
 		 */
 
-		// says unexpected poke to graph-store with mark json,
-		// but when i poke with mark graph-update, it says poke-as cast fail, [%key 'ship']
-		// so my go to guy apparently hasn't implemented it yet either:
-		// ok wtaf I can'tfigure out the differnece between my payloads and stuff ahghghghghgh
 
 		//https://github.com/dclelland/UrsusAPI/blob/master/Sources/UrsusAPI/APIs/Graph/Agents/GraphStoreAgent.swift
 		// rip
 		// okay so we're gonna try to create a graph instead of adding it https://github.com/urbit/urbit/blob/master/pkg/interface/src/logic/api/graph.ts#L109
-
-//		long NOW = 0;
-//		long NOW = Instant.now().toEpochMilli();
-//		JsonObject graphPayload = AirlockUtils.gson.toJsonTree(Map.of(
-//				 https://github.com/urbit/urbit/blob/531f406222c15116c2ff4ccc6622f1eae4f2128f/pkg/interface/src/views/landscape/components/NewChannel.tsx#L98
-//				"create", Map.of(
-//						"resource", new Resource(
-//								"~zod",       // =entity
-//								"test-graph" + NOW    // name=term
-//						),
-//						"title", "Test Graph!!!" + NOW,
-//						"description", "graph for testing only! having fun strictly prohibited",
-//						"associated", Map.of(
-//								"group", Map.of(
-//										"ship", "~zod",
-//										"name", "TEST_GROUP" + NOW
-//								)
-//						),
-//						"module", "link"
-//				)
-//		)).getAsJsonObject();
-//		urbit.spiderRequest("graph-view-action", "graph-create", "json", graphPayload.getAsJsonObject());
 
 
 		// answer was found in lib/resource.hoon. basically, you need a sig int front of the ship's name
