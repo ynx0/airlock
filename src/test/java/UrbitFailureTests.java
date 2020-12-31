f0e97c486bf77f9cd8ab0d3e3d9d09a371a57f0a
Yaseen <yaseen.nvt@gmail.com>
2020-12-31 07:58:48 -0500

    Update comments in tests

--- src/test/java/UrbitFailureTests.java
+++ src/test/java/UrbitFailureTests.java
@@ -7,6 +7,8 @@ public class UrbitFailureTests {
 	// TODO populate with tests which test various failure modes of the api
 
 	// idea: sig vs. no sig on known payloads
+	// scrying or asking for nodes on a graph that doesn't exist
+	// posting to existing graph but not respecting the graph schema for things like publish
 
 //	@BeforeAll
 //	public static void setup() {
