50933fc3bb34f7e1d33b9802974107116728a9a5
Yaseen <yaseen.nvt@gmail.com>
2021-01-11 11:40:43 -0500

    Graph: Finalize  moving utility methods to `Index` and misc renaming

--- src/main/java/airlock/agent/graph/types/Graph.java
+++ src/main/java/airlock/agent/graph/types/Graph.java
@@ -5,11 +5,10 @@ import com.google.gson.*;
 import java.lang.reflect.Type;
 import java.math.BigInteger;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static java.util.stream.StreamSupport.stream;
 
-// same as BigIntOrderedMap
+// same as BigIntOrderedMap<Node>
 public class Graph extends TreeMap<BigInteger, Node> {
 
 
@@ -24,56 +23,8 @@ public class Graph extends TreeMap<BigInteger, Node> {
 		this.putAll(graphMap);
 	}
 
-	/**
-	 * N.B: this method returns a List\<BigInteger\>, not a BigInteger even though it is dealing with a single index
-	 * This is because it is a more general method which is actually dealing with what I'm gonna call "DeepIndex"'s
-	 * (you can also think of it as a multi-level index)
-	 * Basically, they are supposed to model a nested index, such as "/17239874987324798432/4/1"
-	 * which becomes List(BigInt(17239874987324798432), BigInt(4), BigInt(1))
-	 * @param indexStr the index string to parse
-	 * @return the resulting "deep index"
-	 */
-	public static List<BigInteger> indexListFromString(String indexStr) {
-		return Arrays
-				.stream(indexStr.split("/"))
-				.skip(1)
-				.map(BigInteger::new)
-				.collect(Collectors.toList())
-				;
-	}
 
-	public static BigInteger indexFromString(String index) {
-		return indexListFromString(index).get(0); // can be null and bubble up, but we'll cross that bridge when we get to it
-	}
-
-	public static String indexToString(BigInteger index) {
-		return indexListToString(Collections.singletonList(index));
-	}
-
-	public static String indexListToString(List<BigInteger> indexList) {
-		return indexList.stream()       // 1
-				.map(Objects::toString) // 2
-				.collect(Collectors.joining("/", "/", "")) // 3
-				;
-		// 1. For each BigInteger index
-		// 2. convert index to a string
-		// 3. join the resulting string list into one string
-		//    that is separated by slashes, has a slash at the start, and does not have anything at the end
-
-		// example: [17012348792174, 1, 170812937724, 2] -> "/17012348792174/1/170812937724/2"
-
-
-	}
-
-
-	/*
-	// todo potentially use wrapper method like this to clean up other code
-	public BigInteger indexFromString(String indexStr) {
-		return indexListFromString(indexStr).get(0);
-	}
-	*/
-
-	public void addNode(List<BigInteger> deepIndex, Node node) {
+	public void addNode(Index index, Node node) {
 		// adapted from https://github.com/urbit/urbit/blob/598a46d1f7520ed3a2fa990d223b05139a2fe344/pkg/interface/src/logic/reducers/graph-update.js#L98
 		// okay so the code there is confusing, because BigIntOrderedMap.ts is mutable
 		// but they treat it as if it were immutable in this equivalent function
@@ -86,27 +37,27 @@ public class Graph extends TreeMap<BigInteger, Node> {
 		// also, there is a call to graph.set(index, parNode)
 		// which doesn't make sense since the graph already has the node at that index
 		// and Node is mutable so it is already changed ???
-		BigInteger index = deepIndex.get(0);
-		int indexLen = deepIndex.size();
+		BigInteger indexSingleton = index.get(0); // todo come up with a better name
+		int indexLen = index.size();
 		if (indexLen == 1) {
-			this.put(index, node);
+			this.put(indexSingleton, node);
 		} else {
-			if (!this.containsKey(index)) {
+			if (!this.containsKey(indexSingleton)) {
 				// todo see if we want to be this strict or just silently ignore like landscape does
 				throw new IllegalStateException("parent node doesn't exist, can't add child");
 			}
 			// todo test thoroughly
-			Node parentNode = this.get(index);
+			Node parentNode = this.get(indexSingleton);
 			parentNode.ensureChildGraph();
 			assert parentNode.children != null;
-			parentNode.children.addNode(deepIndex.subList(1, indexLen), node);
+			parentNode.children.addNode((Index) index.subList(1, indexLen), node);
 			// this.set(index, parentNode); (see above comment to why this commented out)
 		}
 	}
-	// traverses the graph, trying to find the node specified by the deepIndex
-	public void removeNode(List<BigInteger> deepIndex) {
-		int indexLen = deepIndex.size();
-		BigInteger currentIndex = deepIndex.get(0);
+	// traverses the graph, trying to find the node specified by the index
+	public void removeNode(Index index) {
+		int indexLen = index.size();
+		BigInteger currentIndex = index.get(0);
 
 		if (indexLen == 1) {
 			this.remove(currentIndex);
@@ -115,7 +66,7 @@ public class Graph extends TreeMap<BigInteger, Node> {
 			child.ensureChildGraph();
 
 			if (child.children != null) {
-				child.children.removeNode(deepIndex.subList(1, indexLen));
+				child.children.removeNode((Index) index.subList(1, indexLen));
 			} else {
 				System.out.println("Warning, child graph does not exist, unable to delete node");
 			}
@@ -125,11 +76,6 @@ public class Graph extends TreeMap<BigInteger, Node> {
 			// in the javascript implementation
 			// this.put(currentIndex, child);
 		}
-	}
-
-	// idk if this is actually semantically valid
-	public NodeMap toNodeMap() {
-		return new NodeMap(this);
 	}
 
 	static class Adapter implements JsonSerializer<Graph>, JsonDeserializer<Graph> {
@@ -170,7 +116,7 @@ public class Graph extends TreeMap<BigInteger, Node> {
 						JsonArray graphAsArray = graphJSON.getAsJsonArray();
 						String indexStr = graphAsArray.get(0).getAsString();
 
-						BigInteger index = Graph.indexListFromString(indexStr).get(0);
+						BigInteger index = Index.fromString(indexStr).get(0);
 						Node node = context.deserialize(graphAsArray.get(1).getAsJsonObject(), Node.class);
 						result.put(index, node);
 					});
