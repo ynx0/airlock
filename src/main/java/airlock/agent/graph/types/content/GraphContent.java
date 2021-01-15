166d42dc6c475905efc8eb73949ef09d4194ef6f
Yaseen <yaseen.nvt@gmail.com>
2021-01-15 18:43:17 -0500

    GraphContent: Add documentation

--- src/main/java/airlock/agent/graph/types/content/GraphContent.java
+++ src/main/java/airlock/agent/graph/types/content/GraphContent.java
@@ -1,11 +1,34 @@
 package airlock.agent.graph.types.content;
 
 
+import airlock.agent.graph.types.Node;
 import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
 // ideally this would be a sealed class or something
+
+/**
+ * This abstract class represents the ancestor of all types of GraphContent.
+ * That is, all content types which %graph-store supports within its `content` field.
+ * <p>
+ * <br/>
+ * <br/>
+ * Currently, there are 5 types of content supported:
+ * <ul>
+ *     <li>{@link TextContent} - Text</li>
+ *     <li>{@link ReferenceContent} - Reference to another {@link Node}</li>
+ *     <li>{@link UrlContent} - Any url.</li>
+ *     <li>{@link CodeContent} - Content containing code that was run on a ship and its output</li>
+ *     <li>{@link MentionContent} - A mention to another ship</li>
+ * </ul>
+ * <p>
+ * <br/>
+ * <br/>
+ * Generally speaking, these content types are useful to client so that they may treat them differently and present them in special ways.
+ * For instance, only content within a {@link UrlContent} container get treated as a url in Landscape (and are thus clickable).
+ * Also, creating a TextContent with text "~zod" is not equivalent to creating MentionContent with ship "~zod".
+ */
 public abstract class GraphContent {
 
 	static class Adapter implements JsonDeserializer<GraphContent>, JsonSerializer<GraphContent> {
