e0279bf2d07d917ec2fa93b5cbd6429789b9a694
Yaseen <yaseen.nvt@gmail.com>
2021-01-27 13:35:23 -0500

    Delete deprecated chat agent

--- src/test/java/UrbitUnitTests.java
+++ src/test/java/UrbitUnitTests.java
@@ -1,34 +1,10 @@
-import airlock.AirlockChannel;
-import airlock.agent.chat.ChatUtils;
 import airlock.agent.graph.types.Resource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.time.Instant;
-import java.util.Map;
-
 import static airlock.AirlockUtils.gson;
 
 public class UrbitUnitTests {
-
-	@Test
-	public void chatUtilsCreatesProperPayload() {
-		// todo unit tests
-		String uid = AirlockChannel.uid();
-		String testText = "Hello World";
-		Map<String, Object> targetPayload = Map.of(
-				"message", Map.of(
-						"path", "/~zod/test",
-						"envelope", Map.of(
-								"uid", uid,
-								"number", 1,
-								"author", "~zod",
-								"when", Instant.now().toEpochMilli(),
-								"letter", Map.of("text", testText)
-						)
-				)
-		);
-	}
 
 
 	@Test
@@ -37,33 +13,12 @@ public class UrbitUnitTests {
 	}
 
 
-	@Test
-	public void validMessageFromFactory() {
-		String uid = AirlockChannel.uid();
-		long when = Instant.now().toEpochMilli();
-		Map<String, Object> expectedPayload = Map.of(
-				"message", Map.of(
-						"path", "/~zod/test",
-						"envelope", Map.of(
-								"uid", uid,
-								"number", 1,
-								"author", "~zod",
-								"when", when,
-								"letter", Map.of("text", "TEST")
-						)
-				)
-		);
-		Assertions.assertEquals(gson.toJsonTree(expectedPayload), gson.toJsonTree(ChatUtils.createMessagePayload("/~zod/test", uid, "~zod", when, "TEST")));
-
-	}
 
 	/*
 	//		URL baseURL = new URL("http://localhost:8080/~/").toURI().normalize().toURL();
 //		System.out.println(baseURL);
 //		System.out.println(baseURL.toURI().resolve("/scry//" + "app/" + "/"));
 //		System.out.println(baseURL.toURI().resolve("/~/channel/" + Urbit.generateChannelID()));
-
-
 	 */
 
 
