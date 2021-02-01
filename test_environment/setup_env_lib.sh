52468b848211b34b6965e36bfb1669abfcd6e901
Yaseen <yaseen.nvt@gmail.com>
2021-02-01 01:19:28 -0500

    Test Environment: refactor `setup_env_lib`: all functions generic over any ship -

--- test_environment/setup_env_lib.sh
+++ test_environment/setup_env_lib.sh
@@ -3,15 +3,13 @@
 PLATFORM=linux64
 VERSION_NUM=v1.0-rc1
 URBIT_VERSION=urbit-$VERSION_NUM-$PLATFORM # example output: urbit-v1.0-rc1-linux64
-FAKEZOD_TAR=fakezod-init.tar.gz
-LOGFILE=fakeship_output.log
+FAKEZOD_TAR=init.tar.gz
+LOGFILE=output.log
 OTA_PATH=./urbit
 
 #cd test_environment || exit
 
 ### N.B. all functions in this file should be stateless, and accept any state as parameters
-
-### TODO: make functions generic over ship so that we can spawn fleets of fakeships, defaulting to ~zod
 
 die() {
   echo "$*" 1>&2
@@ -19,7 +17,7 @@ die() {
 }
 
 function downloadUrbitRuntime() {
-  echo "Downloading Urbit Runtime"
+  echo "Downloading Urbit Runtime $URBIT_VERSION"
   curl -o $URBIT_VERSION.tgz https://bootstrap.urbit.org/$URBIT_VERSION.tgz # force filename to be $URBIT_VERSION
   # we need to strip one directory inside and just get the binary files directly,
   # then extract them to the known folder name using the -C flag
@@ -30,96 +28,160 @@ function downloadUrbitRuntime() {
 }
 
 function downloadLatestOTA() {
+  echo "Downloading latest OTA"
   # this function is necessary for fakezods because they are created from the latest boot pill, not the latest ota
   # however, if you are targeting the latest ota, you will not be able to get it because of the fact that you are a fake ship
   # this function clones urbit/urbit
   # # #
 #  curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash  # uncomment for latest version
   sudo apt-get install git-lfs
-  git lfs install --skip-repo  # need lfs for solid.pill
+  git lfs install --skip-repo  # need lfs to properly clone boot pill
   git clone --depth 1 --branch master https://github.com/urbit/urbit $OTA_PATH
+}
+
+function safepatp() {
+  # this function removes any invalid characters from a ship's name
+  # that make it unsuitable for a session name in screen/bash.
+  # this ends up being only the sig character
+  echo "${1//[~]/""}"  # in string $1, substitute any "~" with an empty string
 }
 
 # MARK - live ship management
 function start_ship() {
-  screen -d -m -S fakeship -L -Logfile "$LOGFILE" ./$URBIT_VERSION/urbit zod
+  # $1 = patp of desired ship
+  local SHIP SAFE_SHIP
+  SHIP="$1"
+  SAFE_SHIP=$(safepatp "$SHIP")
+
+  screen -d -m -S "$SAFE_SHIP" -L -Logfile "$SAFE_SHIP-$LOGFILE" ./$URBIT_VERSION/urbit "$SAFE_SHIP"
 }
 
 function send2ship() {
-  # read the manpage for the input format that screen expects
-  screen -S fakeship -p 0 -X stuff "$1"
+  # $1 = patp of desired ship
+  # $2 = input to send
+  local SHIP INPUT SAFE_SHIP
+  SHIP="$1"
+  INPUT="$2"
+  SAFE_SHIP=$(safepatp "$SHIP")
+
+  # this command reads as:
+  # with the session $SAFE_SHIP, window 0, send the stuff command, with the string $INPUT.
+  # read the manpage for the input format that the `stuff` command expects
+  screen -S "$SAFE_SHIP" -p 0 -X stuff "$INPUT"
 }
 
 function getLastNLines() {
+  # $1 = patp of desired ship
+  # $2 = number of lines to get
+  local SHIP N SAFE_SHIP
+  SHIP="$1"
+  N="$2"
+  SAFE_SHIP=$(safepatp "$SHIP")
+
   # shellcheck disable=SC2005
-  echo "$(tail -n"$1" fakeship_output.log)"
+  echo "$(tail -n"$N" "$SAFE_SHIP-$LOGFILE")"
 }
 
 function wait4boot() {
-  echo "Waiting for zod to boot: "
-  until [[ "$(tail -n1 fakeship_output.log)" =~ "~zod:dojo>" ]]; do
-    getLastNLines 2
-    sleep 10s
+  # $1 = patp of desired ship
+  local SHIP SAFE_SHIP
+  SHIP="$1"
+  SAFE_SHIP=$(safepatp "$SHIP")
+
+  echo "Waiting for $SHIP to boot: "
+  until [[ "$(tail -n1 "$SAFE_SHIP-$LOGFILE")" =~ $SHIP":dojo>" ]]; do
+    getLastNLines "$SHIP" 2
+    sleep 3s
   done
 }
 
 function killShip() {
-  screen -S fakeship -X quit 2>/dev/null # ok if it doesn't exist
+  # $1 = patp of desired ship
+  local SHIP SAFE_SHIP
+  SHIP="$1"
+  SAFE_SHIP=$(safepatp "$SHIP")
+
+  screen -X -S "$SAFE_SHIP" quit 2>/dev/null # ok if it doesn't exist
+  # if ship is null then everything will be killed ...
 }
 
 # MARK - ship creation/deletion + boot
 function make_fakezod() {
-  rm -rf ./zod # remove if existing fakezod
-  echo "Creating fakezod"
-  # screen command adapted from https://stackoverflow.com/a/15026227
-  screen -d -m -S fakeship -L -Logfile "$LOGFILE" ./$URBIT_VERSION/urbit -F zod
-  wait4boot
-  echo "Fakezod created"
-  send2ship "^D"
-  sleep 5s
-}
+  # $1 = patp of desired ship
+  # $2 = perform manual ota?
+  local SHIP OTA SAFE_SHIP
+  SHIP="$1"
+  OTA="$2"
+  SAFE_SHIP="$(safepatp "$SHIP")"
 
-function make_fakezod_ota() {
-  # this function is equivalent to the make_fakezod, but uses the boot pill and arvo kernel from `urbit/urbit/master`
-  # the reason for duplication is simply to keep state out of the lib script
-  rm -rf ./zod # remove if existing fakezod
-  echo "Creating fakezod using latest ota"
-  [[ ! -d $OTA_PATH ]] && die "Could not find folder containing urbit repo"
-  screen -d -m -S fakeship -L -Logfile "$LOGFILE" ./$URBIT_VERSION/urbit -F zod -B "urbit/bin/solid.pill" -A "urbit/pkg/arvo"
-  wait4boot
-  echo "Fakezod created"
+  rm -rf "./$SAFE_SHIP" # remove if existing fake ship
+  echo "Creating fake $SHIP"
+
+  # screen command adapted from https://stackoverflow.com/a/15026227
+
+  if [[ $OTA == true ]]; then
+    echo "Using latest ota"
+    [[ ! -d $OTA_PATH ]] && die "Could not find folder containing urbit repo"
+    screen -d -m -S "$SAFE_SHIP" -L -Logfile "$SAFE_SHIP-$LOGFILE" ./$URBIT_VERSION/urbit -F "$SAFE_SHIP" -B "urbit/bin/solid.pill" -A "urbit/pkg/arvo"
+  else
+    screen -d -m -S "$SAFE_SHIP" -L -Logfile "$SAFE_SHIP-$LOGFILE" ./$URBIT_VERSION/urbit -F "$SAFE_SHIP"
+  fi
+
+  wait4boot "$SHIP"
+  sleep 3s  # wait for all fake ships on local network to properly poke/ack. should prevent some errors from smudging up the event log.
+  # but it didn't. weird. getting a poke-ack on nus. todo investigate this later
+  echo "Fake $SHIP created"
   send2ship "^D"
-  sleep 5s
+  sleep 3s
 }
 
 function boot_fakezod() {
-  start_ship
-  wait4boot
-  echo "Booted fakezod"
+  # $1 = patp of desired ship
+  local SHIP
+  SHIP="$1"
+
+  start_ship "$SHIP"
+  wait4boot "$SHIP"
+  echo "Booted fake $SHIP"
 }
 
 function tar_fakezod_state() {
-  echo "Saving pristine fakezod state"
-  if [ -d ./zod ]; then
-    #    rm ./zod/.urb/.http.ports
-    #    rm ./zod/.urb/.vere.lock
-    tar cvzf $FAKEZOD_TAR zod
+  # $1 = patp of desired ship
+  local SHIP SAFE_SHIP
+  SHIP="$1"
+  SAFE_SHIP=$(safepatp "$SHIP")
+
+  echo "Saving pristine fake $SHIP state"
+  if [ -d "./$SAFE_SHIP" ]; then
+#        rm "./$SAFE_SHIP/.urb/.http.ports"
+#        rm "./$SAFE_SHIP/.urb/.vere.lock"
+    tar cvzf "$SAFE_SHIP-$FAKEZOD_TAR" "$SAFE_SHIP"
   else
-    echo "Could not save ./zod. Does not exist"
+    die "Could not save ./$SHIP. Directory does not exist"
   fi
 }
 
 function untar_fakezod_state() {
-  echo "Unzipping existing fakezod"
-  tar xvf ./$FAKEZOD_TAR
+  # $1 = patp of desired ship
+  local SHIP SAFE_SHIP
+  SHIP="$1"
+  SAFE_SHIP=$(safepatp "$SHIP")
+
+  echo "Unzipping existing fake $SHIP"
+  tar xvf ./"$SAFE_SHIP-$FAKEZOD_TAR"
 }
 
 function cleanup() {
-  killShip
+  # $1 = patp of desired ship
+  local SHIP SAFE_SHIP
+  SHIP="$1"
+  SAFE_SHIP=$(safepatp "$SHIP")
+
+  killShip "$SHIP"
   mkdir -p ./old_logs
-  mv "$LOGFILE" "./old_logs/${LOGFILE}_$(date -Iminutes).old.log"
-  rm -rf ./zod >>/dev/null 2>&1
-  rm -f $URBIT_VERSION.tgz
+  mv "$SAFE_SHIP-$LOGFILE" "./old_logs/$SAFE_SHIP-${LOGFILE}_$(date -Iminutes).old.log" >> /dev/null 2>&1
+  rm -rf "./$SAFE_SHIP" >> /dev/null 2>&1  # remove the non-compressed pier
+  rm -f $URBIT_VERSION.tgz  # remove urbit runtime zipfile
 }
 
 # assuming eyre will be live on 8080 b/c port 80 is not available by default
